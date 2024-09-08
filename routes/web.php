0c8a64023a5113ab68dc78c4b2386eea6d84830e
bllyjulian <billyjulian75@gmail.com>
2024-09-08 21:24:47 +0700

    ADD : Peta Rekomendasi Tanaman Pangan

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PetaRekomendasiController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PetaRekomendasiControllerr;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('layout.navigation');
+})->name('dashboard');
+
+Route::get('/cuaca', function () {
+    return view('cuaca.index');
+})->name('cuaca');
+
+Route::get('/peta-rekomendasi', [PetaRekomendasiController::class, 'index'])->name('peta');
+
