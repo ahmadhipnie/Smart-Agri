dbf54ba5e98bedd2a7e10e748f98348e25f3aeec
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2024-10-22 06:05:00 +0700

    Revert "refactor: Enable filter without button"
    This reverts commit 622c6e8e696a20ca63ba5fd21d709e316449655c.

--- app/Http/Controllers/backend/PrediksiCuacaController.php
+++ app/Http/Controllers/backend/PrediksiCuacaController.php
@@ -9,11 +9,6 @@ class PrediksiCuacaController extends Controller
 {
     public function index(Request $request)
     {
-        $yearData = [2024, 2025];
-        $months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 
-                    'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
-                ];
-
         // Daftar provinsi yang tersedia
         $provinceData = [
             'ACEH', 'BALI', 'BANGKA BELITUNG', 'BANTEN', 'BENGKULU', 'GORONTALO', 'JAKARTA', 'JAMBI',
@@ -25,44 +20,67 @@ class PrediksiCuacaController extends Controller
             'SUMATERA SELATAN', 'SUMATERA UTARA', 'YOGYAKARTA'
         ];
 
+        // Tahun yang tersedia
+        $yearData = [2024, 2025];
+
         // Ambil input dari form, dengan default untuk pertama kali halaman dibuka
-        $prov = $request->input('prov', 'JAWA TIMUR');
-        $date = $request->input('date', 1);
-        $bulan = $request->input('month', 1);
-        $year = $request->input('year', 2024);
+        $provinceSelect = $request->input('provinsiSelect', 'JAWA TIMUR');
+        $tanggal = $request->input('tanggalSelect', 1);
+        $bulan = $request->input('bulanSelect', 1);
+        $year = $request->input('tahunSelect', 2024);
 
         // Menyiapkan array untuk menampung data cuaca
         $weatherData = [];
-        $keys = ['temp', 'cloudcover', 'humidity', 'precip', 'windspeed', 'sealevelpressure'];
 
         // Loop melalui setiap bulan untuk mendapatkan data cuaca
         for ($month = 1; $month <= 12; $month++) {
             // Request ke API untuk mengambil data prediksi cuaca
             $response = Http::retry(3, 1000)->timeout(60)->get("http://103.210.69.119:5000/predictions", [
-                'name' => $prov,
-                'tanggal' => $date,
+                'name' => $provinceSelect,
+                'tanggal' => $tanggal,
                 'bulan' => $month,
                 'tahun' => $year,
             ]);
 
+            // Jika API merespon dengan sukses, masukkan data ke dalam array
             if ($response->successful() && !empty($response->json()) && isset($response->json()[0])) {
                 $data = $response->json()[0];
-
-                foreach ($keys as $key) {
-                    $weatherData[$month][$key] = number_format($data[$key], 1);
-                }
-
+                $weatherData[$month] = [
+                    'temp' => number_format($data['temp'] ?? null, 1), // Membulatkan suhu ke 1 desimal
+                    'cloudcover' => number_format($data['cloudcover'] ?? null, 1), // Membulatkan tutup awan ke 1 desimal
+                    'humidity' => number_format($data['humidity'] ?? null, 1), // Membulatkan kelembapan ke 1 desimal
+                    'precip' => number_format($data['precip'] ?? null, 1), // Membulatkan presipitasi ke 1 desimal
+                    'windspeed' => number_format($data['windspeed'] ?? null, 1), // Membulatkan kecepatan angin ke 1 desimal
+                    'sealevelpressure' => number_format($data['sealevelpressure'] ?? null, 1), // Membulatkan tekanan udara ke 1 desimal
+                ];
             } else {
-                foreach ($keys as $key) {
-                    $weatherData[$month][$key] = null;
-                }
+                // Jika data tidak tersedia, isi dengan null
+                $weatherData[$month] = [
+                    'temp' => null,
+                    'cloudcover' => null,
+                    'humidity' => null,
+                    'precip' => null,
+                    'windspeed' => null,
+                    'sealevelpressure' => null,
+                ];
             }
-            
         }
 
         // Mengembalikan data ke view
         return view('backend.cuaca.index', compact(
-            'weatherData', 'year', 'prov', 'provinceData', 'yearData', 'date', 'bulan', 'months'
+            'weatherData', 'year', 'provinceSelect', 'provinceData', 'yearData', 'tanggal', 'bulan'
         ));
+    }
+
+    // Fungsi untuk menangani POST request dan redirect ke index
+    public function fetch(Request $request)
+    {
+        // Redirect ke index dengan input yang dipilih
+        return redirect()->route('cuaca.index', [
+            'provinsiSelect' => $request->input('provinsiSelect'),
+            'tanggalSelect' => $request->input('tanggalSelect'),
+            'bulanSelect' => $request->input('bulanSelect'),
+            'tahunSelect' => $request->input('tahunSelect'),
+        ]);
     }
 }
